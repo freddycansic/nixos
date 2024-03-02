e94f4ef8da22b0c481dfbfaf4f850213a0510b9e
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2024-03-02 21:56:05 +0000

    38 current  2024-03-02 21:56:01  23.11.20240301.79baff8   6.1.79

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -128,8 +128,6 @@
     unzip
     alejandra
     libnotify
-    gnupg
-    pinentry
     helix
     git
     zsh
@@ -137,6 +135,24 @@
 
   programs.zsh.enable = true;
 
+  programs.hyprland = {
+    enable = true;
+    enableNvidiaPatches = true;
+    xwayland.enable = true;
+  };
+
+  programs.gnupg.agent.enable = true;
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware = {
+    opengl.enable = true;
+    nvidia.modesetting.enable = true;
+  }
+  
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
