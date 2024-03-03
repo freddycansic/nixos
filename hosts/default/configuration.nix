c994b247d6a8c41b25bd0efbfa427de6077896c7
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2024-03-03 17:21:28 +0000

    53 current  2024-03-03 17:21:23  23.11.20240301.79baff8   6.1.79

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -67,16 +67,17 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  security.rtkit.enable = true;
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
+    # jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
