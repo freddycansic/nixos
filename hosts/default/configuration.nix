84da626b826c643e52ade7d415d32e40b5a8f1ee
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2024-03-03 21:31:33 +0000

    59 current  2024-03-03 21:31:29  23.11.20240301.79baff8   6.1.79

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -120,11 +120,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    wget
     unzip
     alejandra
     libnotify
