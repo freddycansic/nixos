c21fa1b30db283c31188ac01e0f28bd66ef3bc0e
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2024-04-01 12:11:34 +0100

    171 current  2024-04-01 12:11:30  23.11.20240305.880992d  6.1.80

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -119,8 +119,8 @@
   ];
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "freddy";
+  # services.xserver.displayManager.autoLogin.enable = true;
+  # services.xserver.displayManager.autoLogin.user = "freddy";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   # systemd.services."getty@tty1".enable = false;
