c1b996ccb6b3ba5bc4282fa672626311237d5236
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2024-04-02 11:33:18 +0100

    190 current  2024-04-02 11:33:13  23.11.20240305.880992d  6.1.80

--- modules/nvim/nvim.nix
+++ modules/nvim/nvim.nix
@@ -1,11 +1,15 @@
-{config, ...}: let
-  nvchad = builtins.fetchGit {
-    url = "https://github.com/NvChad/NvChad.git";
-    rev = "6fb5c313edc966f187c7483a16affaec0518b641";
-  };
-in {
-  home.file."~/.config/nvim" = {
-    source = nvchad;
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.file."${config.home.homeDirectory}/.config/nvim" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "NvChad";
+      repo = "NvChad";
+      rev = "6fb5c313edc966f187c7483a16affaec0518b641";
+      sha256 = "sha256-U81M3RFMP7jKirxj3ROCsyqTRXGCrtN6VsPrewlPSLI=";
+    };
     recursive = true;
   };
 }
