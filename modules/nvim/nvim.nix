33f775b9d0ae0eb4593df5efc9aee5bd8fa6b504
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2024-04-03 17:54:34 +0100

    194 current  2024-04-03 17:54:29  24.05.20240329.d8fe5e6  6.6.23

--- modules/nvim/nvim.nix
+++ modules/nvim/nvim.nix
@@ -1,12 +1,5 @@
 {
-  config,
   pkgs,
+  stdenv,
   ...
-}: {
-  xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
-    owner = "NvChad";
-    repo = "NvChad";
-    rev = "6fb5c313edc966f187c7483a16affaec0518b641";
-    sha256 = "sha256-U81M3RFMP7jKirxj3ROCsyqTRXGCrtN6VsPrewlPSLI=";
-  };
-}
+}: {}
