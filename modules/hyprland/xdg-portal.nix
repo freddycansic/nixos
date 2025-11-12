4f9b4eac46c098b1e28d233c720f3c3bf5b0abc9
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2025-11-12 20:57:53 +0000

    20:57:26
    25.11.20251102.b3d51a0    6.12.57

--- modules/hyprland/xdg-portal.nix
+++ modules/hyprland/xdg-portal.nix
@@ -13,10 +13,10 @@
 
   services.xserver.updateDbusEnvironment = true;
 
-  home-manager.users.freddy = {
-    home.file."${config.users.users.freddy.home}/.config/systemd/user.conf".text = ''
-      [Manager]
-      ManagerEnvironment="XDG_DATA_DIRS=/usr/local/share:/usr/share:/home/${config.users.users.freddy.name}/.local/state/nix/profile/share:/nix/var/nix/profiles/default/share"
-    '';
-  };
+  # home-manager.users.freddy = {
+  #   home.file."${config.users.users.freddy.home}/.config/systemd/user.conf".text = ''
+  #     [Manager]
+  #     ManagerEnvironment="XDG_DATA_DIRS=/usr/local/share:/usr/share:/home/${config.users.users.freddy.name}/.local/state/nix/profile/share:/nix/var/nix/profiles/default/share"
+  #   '';
+  # };
 }
