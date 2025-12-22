0a1a263399670542b412e02e6ae22ab14ac56dd2
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2025-12-22 17:01:33 +0000

    17:01:22 - add devenv public key stuff
    26.05.20251218.c6245e8    6.12.62

--- hosts/common-system.nix
+++ hosts/common-system.nix
@@ -32,6 +32,11 @@
     options = "--delete-older-than 30d";
   };
 
+  nix.extraOptions = ''
+    extra-substituters = https://devenv.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+  '';
+
   # Enable networking
   networking.networkmanager.enable = true;
 
