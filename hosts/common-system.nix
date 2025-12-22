b67d36ed2d1ea3a060e4b6ddf98c531273596938
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2025-12-22 10:47:17 +0000

    10:46:05 - gc
    26.05.20251127.2fad6ea    6.12.59

--- hosts/common-system.nix
+++ hosts/common-system.nix
@@ -26,6 +26,12 @@
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   # Enable networking
   networking.networkmanager.enable = true;
 
