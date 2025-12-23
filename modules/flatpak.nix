5809f8af045bd2e9639987fb2988f37114dd4b02
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2025-12-23 14:18:09 +0000

    14:18:00 - add spotify desktop entry
    26.05.20251218.c6245e8 6.12.62

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -14,8 +14,18 @@
   };
 
   home-manager.users.freddy = {
+    # add flatpak to path
     home.file.".profile".text = ''
       XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"
     '';
+
+    xdg.desktopEntries.spotify-flatpak = {
+      name = "Spotify";
+      genericName = "Music Player";
+      exec = "flatpak run com.spotify.Client";
+      icon = "com.spotify.Client";
+      terminal = false;
+      categories = ["Audio" "Music" "Player"];
+    };
   };
 }
