9a08b9cabe79b91c90ebe2c618ef1130aef441fc
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2026-01-01 19:36:58 +0000

    2026-01-01 19:25:08 - systemd timeout
    26.05.20251218.c6245e8    6.12.62

--- hosts/common-system.nix
+++ hosts/common-system.nix
@@ -15,7 +15,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = null;
+  boot.loader.timeout = -1;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
