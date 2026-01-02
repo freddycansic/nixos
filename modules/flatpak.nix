ee63ac166c17973dd13908e35e1be385ce0d30ff
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2026-01-02 17:37:34 +0000

    17:37:23
    26.05.20251218.c6245e8    6.12.62

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -17,19 +17,19 @@
     "com.spotify.Client"
   ];
 
-  # home-manager.users.freddy = {
-  #   # add flatpak to path
-  #   home.file.".profile".text = ''
-  #     XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"
-  #   '';
+  home-manager.users.freddy = {
+    # add flatpak to path
+    home.file.".profile".text = ''
+      XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"
+    '';
 
-  #   xdg.desktopEntries.spotify-flatpak = {
-  #     name = "Spotify";
-  #     genericName = "Music Player";
-  #     exec = "flatpak run com.spotify.Client";
-  #     icon = "com.spotify.Client";
-  #     terminal = false;
-  #     categories = ["Audio" "Music" "Player"];
-  #   };
-  # };
+    xdg.desktopEntries.spotify-flatpak = {
+      name = "Spotify";
+      genericName = "Music Player";
+      exec = "flatpak run com.spotify.Client";
+      icon = "com.spotify.Client";
+      terminal = false;
+      categories = ["Audio" "Music" "Player"];
+    };
+  };
 }
