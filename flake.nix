133c6786fa69ef044be78098d9481a4b0993b4eb
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2026-01-02 17:36:27 +0000

    17:36:08 - add nix-flatpak for declarative flatpak pkgs
    26.05.20251218.c6245e8    6.12.62

--- flake.nix
+++ flake.nix
@@ -13,36 +13,34 @@
 
     sf-mono-nerd-font.url = "github:austinliuigi/sf-mono-nerd-font";
 
-    # secure boot
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.3";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
   };
 
   outputs = {
     self,
     nixpkgs,
-    lanzaboote,
+    nix-flatpak,
     ...
   } @ inputs: {
-    nixosConfigurations = {
+    nixosConfigurations = let
+      common-modules = [inputs.home-manager.nixosModules.default nix-flatpak.nixosModules.nix-flatpak];
+    in {
       laptop = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/laptop/configuration.nix
-          inputs.home-manager.nixosModules.default
-          lanzaboote.nixosModules.lanzaboote
-        ];
+        modules =
+          [
+            ./hosts/laptop/configuration.nix
+          ]
+          ++ common-modules;
       };
 
       pc = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/pc/configuration.nix
-          inputs.home-manager.nixosModules.default
-          lanzaboote.nixosModules.lanzaboote
-        ];
+        modules =
+          [
+            ./hosts/pc/configuration.nix
+          ]
+          ++ common-modules;
       };
     };
   };
--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -4,28 +4,32 @@
   inputs,
   ...
 }: {
-  services.flatpak.enable = true;
-  systemd.services.flatpak-repo = {
-    wantedBy = ["multi-user.target"];
-    path = [pkgs.flatpak];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
+  # services.flatpak.enable = true;
+  # systemd.services.flatpak-repo = {
+  #   wantedBy = ["multi-user.target"];
+  #   path = [pkgs.flatpak];
+  #   script = ''
+  #     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  #   '';
+  # };
 
-  home-manager.users.freddy = {
-    # add flatpak to path
-    home.file.".profile".text = ''
-      XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"
-    '';
+  services.flatpak.packages = [
+    "com.spotify.Client"
+  ];
 
-    xdg.desktopEntries.spotify-flatpak = {
-      name = "Spotify";
-      genericName = "Music Player";
-      exec = "flatpak run com.spotify.Client";
-      icon = "com.spotify.Client";
-      terminal = false;
-      categories = ["Audio" "Music" "Player"];
-    };
-  };
+  # home-manager.users.freddy = {
+  #   # add flatpak to path
+  #   home.file.".profile".text = ''
+  #     XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"
+  #   '';
+
+  #   xdg.desktopEntries.spotify-flatpak = {
+  #     name = "Spotify";
+  #     genericName = "Music Player";
+  #     exec = "flatpak run com.spotify.Client";
+  #     icon = "com.spotify.Client";
+  #     terminal = false;
+  #     categories = ["Audio" "Music" "Player"];
+  #   };
+  # };
 }
