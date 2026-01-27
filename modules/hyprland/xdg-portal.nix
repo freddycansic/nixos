e1d5c68377b17415adcb9032f87d45f9661093da
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2026-01-27 17:54:24 +0000

    17:53:53 - xdg-desktop-portal-wlr
    26.05.20260121.88d3861    6.12.66

--- modules/hyprland/xdg-portal.nix
+++ modules/hyprland/xdg-portal.nix
@@ -8,8 +8,10 @@
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
-    extraPortals = [pkgs.xdg-desktop-portal-gtk];
-    config.common.default = ["gtk"];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-wlr
+    ];
   };
 
   services.xserver.updateDbusEnvironment = true;
