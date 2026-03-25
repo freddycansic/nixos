39a78979bcad7168cab7d58505abe1c603a9f5e6
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2026-03-25 22:00:28 +0000

    22:00:12 - disable hyprland default wallpaper
    26.05.20260121.88d3861 6.12.66

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -184,8 +184,8 @@
           };
 
           misc = {
-            force_default_wallpaper = -1;
-            disable_hyprland_logo = false;
+            disable_hyprland_logo = true;
+            disable_splash_rendering = true;
           };
 
           input = {
