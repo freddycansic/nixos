d74d364fad34b4c3ab258430bacc9ce308487cff
Fred <freddy@cansick.com>
2026-08-04 21:29:40 +0100

    26.11.20260702.6517942 - enable pipewire alsa stuff
    6.18.37 22.5 GB

--- hosts/common/audio.nix
+++ hosts/common/audio.nix
@@ -13,7 +13,7 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = false;
+    alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
