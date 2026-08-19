57695325b5640203b934324a5fcb4c6a16835134
Fred <freddy@cansick.com>
2026-08-19 13:55:47 +0100

    26.11.20260702.6517942
    6.18.37 22.9 GB

--- modules/hyprland/xdg-portal.nix
+++ modules/hyprland/xdg-portal.nix
@@ -5,31 +5,28 @@
   lib,
   ...
 }: {
-  environment.systemPackages = [
-    pkgs.xdg-desktop-portal-gtk
-  ];
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-    ];
-    xdgOpenUsePortal = true;
-    configPackages = [config.programs.hyprland.package];
-    config.hyprland = {
-      default = [
-        "hyprland"
-        "gtk"
-      ];
-      "org.freedesktop.impl.portal.OpenURI" = "gtk";
-      "org.freedesktop.impl.portal.FileChooser" = "gtk";
-      "org.freedesktop.impl.portal.Print" = "gtk";
+  home-manager.users.freddy = {
+    xdg.portal = {
+        enable = true;
+        extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        ];
+        xdgOpenUsePortal = true;
+        configPackages = [config.programs.hyprland.package];
+        config.hyprland = {
+        default = [
+            "hyprland"
+            "gtk"
+        ];
+        "org.freedesktop.impl.portal.OpenURI" = "gtk";
+        "org.freedesktop.impl.portal.FileChooser" = "gtk";
+        "org.freedesktop.impl.portal.Print" = "gtk";
+        };
     };
   };
 
   services.xserver.updateDbusEnvironment = true;
 
-  # home-manager.users.freddy = {
   #   home.file."${config.users.users.freddy.home}/.config/systemd/user.conf".text = ''
   #     [Manager]
   #     ManagerEnvironment="XDG_DATA_DIRS=/usr/local/share:/usr/share:/home/${config.users.users.freddy.name}/.local/state/nix/profile/share:/nix/var/nix/profiles/default/share"
