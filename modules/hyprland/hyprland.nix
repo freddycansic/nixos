4bea2b913304c09ad0c7dcfd8e70cbd68cf37c41
Fred <freddy@cansick.com>
2026-08-19 13:11:16 +0100

    26.11.20260702.6517942 - make sure hyprland home manager config is using nixos modules
    6.18.37 22.9 GB

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -151,6 +151,9 @@ in {
 
       wayland.windowManager.hyprland = {
         enable = true;
+        # set the Hyprland and XDPH packages to null to use the ones from the NixOS module
+        package = null;
+        portalPackage = null;
 
         configType = "hyprlang";
 
