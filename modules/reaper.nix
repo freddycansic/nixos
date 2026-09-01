9604780533523a1d68cef8ba30d66fb4b10920ab
Fred <freddy@cansick.com>
2026-09-01 19:23:41 +0100

    26.11.20260819.ffb3c9b - remove .deskop manually...
    6.18.44 26.8 GB

--- modules/reaper.nix
+++ modules/reaper.nix
@@ -1,14 +1,22 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  reaper-no-desktop = pkgs.symlinkJoin {
+    name = "reaper-no-desktop";
+    paths = [pkgs.reaper];
+    postBuild = ''
+      rm -rf $out/share/applications
+    '';
+  };
+in {
   environment.systemPackages = [
     pkgs.pipewire.jack
   ];
 
   home-manager.users.freddy = {
-    home.packages = [pkgs.reaper];
+    home.packages = [reaper-no-desktop];
 
-    xdg.desktopEntries.jeff-reaper = {
+    xdg.desktopEntries.cockos-reaper = {
       name = "REAPER";
-      exec = "${pkgs.pipewire.jack}/bin/pw-jack ${pkgs.reaper}/bin/reaper";
+      exec = "${pkgs.pipewire.jack}/bin/pw-jack ${reaper-no-desktop}/bin/reaper";
     };
   };
 }
